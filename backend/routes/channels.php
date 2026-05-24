e8b29baafe012274a433a06dfa263cf289d20c84
Kazi Omar Faruk <kaziomar144@gmail.com>
2026-05-24 15:58:38 +0600

    feat: user online and offline status broadcast added

--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -11,3 +11,8 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 Broadcast::channel('conversations.{conversation}', function ($user, Conversation $conversation) {
     return $conversation->users->contains($user);
 }, ['guards' => ['sanctum']]);
+
+//online/ offline status
+Broadcast::channel('online', function ($user) {
+    return ['id' => $user->id, 'name' => $user->name];
+}, ['guards' => ['sanctum']]);
